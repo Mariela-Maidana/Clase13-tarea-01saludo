a555bdb7d986db13bd00f46c6618ae86e6c7cf17
marumaidana <maruu.mariela@gmail.com>
2020-08-11 21:32:49 -0300

    Clase 13-saludo

---
+++ index.js
@@ -0,0 +1,5 @@
+let respuestaDeNombre = prompt("Decime tu nombre")
+
+let respuestaDeApellido = prompt("Decime tu apellido")
+
+alert("Hola " + respuestaDeNombre + " " + respuestaDeApellido + ", bienvenida a Ada!")
